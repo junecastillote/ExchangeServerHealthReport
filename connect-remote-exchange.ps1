258c59c7a5ca7616908efe3785dd96336d849036
June Castillote <june.castillote@gmail.com>
2024-08-25 16:35:09 +0800

    Update connect-remote-exchange.ps1

--- connect-remote-exchange.ps1
+++ connect-remote-exchange.ps1
@@ -1,1 +1,1 @@
-. "C:\Program Files\Microsoft\Exchange Server\V15\Bin\RemoteExchange.ps1"; Connect-ExchangeServer -auto;
+. "$($env:ExchangeInstallPath)\Bin\RemoteExchange.ps1"; Connect-ExchangeServer -auto;
