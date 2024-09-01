940f7b7959c4079f1cc5a616e0cf2e9e28ccf080
June Castillote <june.castillote@gmail.com>
2024-09-02 07:04:56 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,11 @@
 # Start Exchange PowerShell implicit remoting session
 . "$($env:ExchangeInstallPath)\Bin\RemoteExchange.ps1"; Connect-ExchangeServer -auto
 
-# Run the server health report
+# Get this script's current parent folder.
 $script_root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+<#
+In this example, the configuration file used is config.psd1,
+which is on the same folder as this script.
+#>
 & $script_root\Get-ExchangeServerHealth.ps1 -ConfigFile $script_root\config.psd1
